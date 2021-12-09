f2eb629af68415e621cab6e115ac57f2f088548b
Rached BENKHALIFA <rached.bkhalifa@gmail.com>
2021-12-09 14:56:46 +0100

    new branch

--- src/App.js
+++ src/App.js
@@ -1,82 +1,176 @@
-import logo from './logo.svg';
-import './App.css';
-import List from './components/list/List';
-import Form from './components/form/Form';
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState } from "react"
+import "./App.css"
+import TaskForm from "./components/taskForm/TaskForm"
+import TasksList from "./components/tasksList/TasksList"
+import { 
+    fetchTasks,
+    addTask as addTaskService,
+    deleteTask as deleteTaskService,
+    updateTask as updateTaskService,
+    fetchTasksByFilter,
+
+    } from "./services/tasks.service"
 
 function App() {
-  const [ isVisible, setIsVisible] = useState(true);
+  const [isError, setIsError] = useState(false)
+  const [searchValue, setSearchValue] = useState("")
+  const [isVisible, setIsVisible] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [newTasks, setNewTasks] = useState([])
   const toggleVisibility = () => {
-    setIsVisible(!isVisible);
+    setIsVisible(!isVisible)
   }
-  const  [tasks, setTasks] = useState([
-    {
-        id: 1,
-        title: "Titre1",
-        duration: 25,
-        type: "Type 1",
-        date: "05-05-2022"
-    },
-    {
-        id: 2,
-        title: "T2",
-        duration: 25,
-        type: "Type 2",
-        date: "05-05-2022"
-    },
-    {
-        id: 3,
-        title: "T3",
-        duration: 25,
-        type: "Type 2",
-        date: "05-05-2022"
-    },
-    {
-        id: 4,
-        title: "T4",
-        duration: 25,
-        type: "Type 3",
-        date: "05-05-2022"
-    }
-])
-const sayHello = () => { console.log("Hello")}
-function sayHello2(){
-  console.log('hello 2')
-}
-const addTask = (title) =>{
-  const newTask = {id:"" + tasks.length + 1, title: title, duration: 15, type: "test", date: "22-10-2022"};
-  setTasks(tasks.concat(newTask))
-}
-const deleteTask = (id) =>{
-  setTasks(tasks.filter(task => task.id != id))
-}
-const updateTask = (newtask) =>{
-  // first solution
-  setTasks(tasks.map(task => {
-    if(task.id == newtask.id){
-      return newtask
-    }
-    return task
-  }))
-}
-  return (
-    <div className="App">
-      <div className="toggle">
-      {/* 1ere solution */}
-        <button onClick={ ()=> toggleVisibility()}>Toggle Visibility</button>
-         {/* 2eme solution */}
-         <button onClick={toggleVisibility}>Toggle Visibility 2</button>
-      </div>
-      {isVisible && (
-        <div>
-          <Form sayHello={sayHello} addTask = {addTask} />
-        </div>
-      )}
+
+  const [tasks, setTasks] = useState([
     
-     <List tasks = {tasks} deleteTask = {deleteTask} updateTask = {updateTask}/>
+  ])
+
+  const addTask = async (title, duration) => {
+    try{
+      setLoading(true)
+      const newTask = await addTaskService({
+        title,
+        duration
+      })
+      setTasks([...tasks, newTask])
+      setLoading(false)
+
+    }catch(e){
+      setLoading(false)
+      console.log("error in addTask")
+    }
+    
+  }
+  const deleteTask = async (id) => {
+
+    // setTasks(newTasks)
+
+    try{
+      setLoading(true)
+      await deleteTaskService(id)
+      const newTasks = tasks.filter((task) => task.id !== id)
+      setTasks(newTasks)
+      setLoading(false)
+
+    }catch(e){
+      setLoading(false)
+      console.log("error in delete Task")
+    }
+  }
+
+  const updateTask = async (id, title, duration) => {
+    
+    
+
+    try{
+      setLoading(true)
+      const newTask = await updateTaskService(id, {title, duration} )
+      const newTasks = tasks.map((task) => task.id == id? newTask: task)
+      setTasks(newTasks)
+      setLoading(false)
+
+    }catch(e){
+      setLoading(false)
+      console.log("error in delete Task")
+    }
+  }
+  // first method
+  // async function fetchData(){
+  //   const tasks = await fetchTasks()
+  //   setTasks(tasks)
+  // }
+  // useEffect(
+  //   () => {
+  //     fetchData()
+  //     console.log(" with useeffect")
+  //   }
+  // )
+
+   // Second method
+  
+  // useEffect(
+  //   () => {
+
+  // async function fetchData(){
+  //   setLoading(true)
+
+  //   try{
+  //     const result = await fetchTasks()
+  //     setTasks(result)
+  //     setLoading(false)
+  //   }catch(e){
+  //     setLoading(false)
+  //     setIsError(true)
+      
+  //   }
+    
+  // }
+  //     fetchData()
+  //     console.log(" with useeffect")
+  //   }, []
+  // )
+  
+  useEffect(
+    () => {
+      let didCancel = false ; 
+  async function fetchData(){
+    setLoading(true)
+    
+    if(searchValue.length === 0){
+    setTasks([])
+    setLoading(false)
+    
+  }else{
+    try{
+      const result = await fetchTasksByFilter(searchValue)
+      if(!didCancel){
+        setTasks(result)
+      setLoading(false)
+      }
+      
+    }catch(e){
+      setLoading(false)
+      setIsError(true)
+      
+    }
+  }
+}
+      fetchData()
+      console.log(" with useeffect")
+      return () =>{
+        console.log("cleanup: ", searchValue)
+        didCancel = true
+      }
+    }, [searchValue]
+  )
+  return (
+    <div className="app">
+      <div className="toggle">
+        <button onClick={toggleVisibility}>Toggle visibility</button>
+      </div>
+      <div>
+        <input type="text" searchValue={searchValue} onChange={(e) => setSearchValue(e.target.value)} align = "right" />
+      </div>
+      <div>
+        {loading && <div>   
+              <div className="Gif">
+                  <img src="img/loading-bar.gif" width="833" alt="Loading Bar GIF - Loading Bar Waiting GIFs"  /> 
+              </div>
+            </div>}
+        {!loading && isVisible && (
+          <div>
+            <TaskForm addTask={addTask}  />
+            <TasksList
+              tasks={tasks}
+              deleteTask={deleteTask}
+              updateTask={updateTask}
+            />
+          </div>
+        )}
+        {isError && <div> Erreur lors de chargement</div>}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -1,57 +1,50 @@
-import React, { useState } from 'react';
-import './task.css';
-import { Button } from 'react-bootstrap';
-import { Panel } from 'react-bootstrap';
-function Task(props) {
-  const handleDeleteTask = () => { props.deleteTask(props.id)}
-  const [isUpdateMode, setIsUpdateMode] = useState(false)
-  const [taskToUpdate, setTaskToUpdate] = useState({id: props.id, title: "", duration: 0})
-
-  const handleUpdateTask = () =>{
-    props.updateTask({id: props.id, title: taskToUpdate.title, duration: taskToUpdate.duration })
-    setIsUpdateMode(false)
+import React, { useState } from "react"
+import "./Task.css"
+export default function Task({ id, title, duration, deleteTask, updateTask }) {
+  const [updateMode, setUpdateMode] = useState(false)
+  const [titleToUpdate, setTitleToUpdate] = useState(title)
+  const [durationToUpdate, setDurationToUpdate] = useState(duration)
+  const handleUpdateTask = () => {
+    updateTask(id, titleToUpdate, durationToUpdate)
+    setUpdateMode(false)
   }
 
-  
   return (
-    <div  class="panel panel-default" >
-{!isUpdateMode ? (
-  <>
-<div   class="panel-body">
- 
-  
-    <ul>
-       <li>id : {props.id}</li> 
-      <li>Title : {props.title}</li>
-        <li>duration :{props.duration}</li>
-        <li>type :{props.type} </li>
-        <li> date :{props.date}</li>
-    </ul>
-</div>
-<div class="actions">
-<Button className="button" class="btn btn-danger" onClick={handleDeleteTask}>delete</Button>
-<Button className="button" class="btn btn-primary" onClick={()=> setIsUpdateMode(true)}>update</Button>
-</div>
-</>
-):(
-<div>
-<input  type="text" 
-        name="title" 
-        value={taskToUpdate.title}
-        onChange={(e) => setTaskToUpdate({...taskToUpdate, title:e.target.value})} />
-<input type="number" 
-        name="duration" 
-        value={taskToUpdate.duration}
-        onChange={(e) => setTaskToUpdate({...taskToUpdate, duration: e.target.value})} />
-<Button className="button" class="btn btn-danger" onClick={handleUpdateTask}>update</Button>
-
-
-</div>
-)
-
-}
-    
-    </div> );
+    <div className="task">
+      {!updateMode ? (
+        <>
+          <div>
+            <div className="title">
+              {title} ({duration} mn)
+            </div>
+          </div>
+          <div className="actions">
+            <div>
+              <button onClick={() => deleteTask(id)}>delete</button>
+              <button onClick={() => setUpdateMode(true)}>update</button>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={titleToUpdate}
+            onChange={(e) => setTitleToUpdate(e.target.value)}
+          />
+          <input
+            type="number"
+            value={durationToUpdate}
+            name="duration"
+            onChange={(e) => setDurationToUpdate(e.target.value)}
+          />
+          <button className="button" onClick={handleUpdateTask}>
+            Update a task
+          </button>
+        </div>
+      )}
+    </div>
+  )
 }
 
-export default Task;
